a0f9c266b7991726ae71e8740d3e1f2b9baf022f
fravaccaro <fravaccaro90@gmail.com>
2015-12-09 00:48:13 +0100

    Extensions support added

--- README.md
+++ README.md
@@ -1,19 +1,13 @@
-<<<<<<< HEAD
 # DynCal
 
-=======
->>>>>>> c6234cee16cd4c6023e5d96a95d9f3e6b04848a7
 Your calendar icon will show you the current day of the month.
 
 Hello everybody! I'm looking for an alternative way daemonize the service, as it's not triggered while Jolla is in deep-sleep. Suggestions are highly appreciated.
 
 For the latest release, go to https://openrepos.net/content/fravaccaro/dynamic-calendar-icon
-<<<<<<< HEAD
 
 ## Extensions' support
 
 You can simply customize the icons (to change icon pack or add your own holidays) by following the following steps:
 
 [TODO]
-=======
->>>>>>> c6234cee16cd4c6023e5d96a95d9f3e6b04848a7
--- usr/share/harbour-dyncal/harbour-dyncal-uninstall.sh
+++ usr/share/harbour-dyncal/harbour-dyncal-uninstall.sh
@@ -2,35 +2,20 @@
 #This script is written by fravaccaro fravaccaro@jollacommunity.it
 #If you want to reuse it, please don't remove this notice
 
-<<<<<<< HEAD
 rm -rf /usr/share/themes/jolla-ambient/meegotouch/z1.0/icons/icon-launcher-calendar.png
 cp /usr/share/harbour-dyncal/icon-launcher-calendar.png /usr/share/themes/jolla-ambient/meegotouch/z1.0/icons/icon-launcher-calendar.png
 rm -rf /usr/share/applications/jolla-calendar.desktop
 /usr/bin/desktop-file-install /usr/share/applications/jolla-calendar.desktop
-=======
-desktop-file-install /usr/share/applications/jolla-calendar.desktop
-cp /usr/share/harbour-dyncal/default.png /usr/share/themes/jolla-ambient/meegotouch/z1.0/icons/icon-launcher-calendar.png
-rm -rf /usr/share/applications/jolla-calendar.desktop
->>>>>>> c6234cee16cd4c6023e5d96a95d9f3e6b04848a7
 echo '[Desktop Entry]
 Type=Application
 Name=Calendar
 X-MeeGo-Logical-Id=calendar-ap-name
 X-MeeGo-Translation-Catalog=calendar
-<<<<<<< HEAD
-=======
-Icon=icon-launcher-calendar
->>>>>>> c6234cee16cd4c6023e5d96a95d9f3e6b04848a7
 Exec=invoker --type=silica-qt5 -n -d 5 -s /usr/bin/jolla-calendar
 Comment=Jolla calendar
 X-Maemo-Service=com.jolla.calendar.ui
 X-Maemo-Object-Path=/com/jolla/calendar/ui
 X-Maemo-Method=com.jolla.calendar.ui.activateWindow
 X-Desktop-File-Install-Version=0.20' > /usr/share/applications/jolla-calendar.desktop
-<<<<<<< HEAD
 /usr/bin/desktop-file-install /usr/share/applications/jolla-calendar.desktop --set-icon=icon-launcher-calendar
 exit 0
-=======
-desktop-file-install /usr/share/applications/jolla-calendar.desktop --set-icon=/usr/share/themes/jolla-ambient/meegotouch/z1.0/icons/icon-launcher-calendar.png
-exit 0
->>>>>>> c6234cee16cd4c6023e5d96a95d9f3e6b04848a7
