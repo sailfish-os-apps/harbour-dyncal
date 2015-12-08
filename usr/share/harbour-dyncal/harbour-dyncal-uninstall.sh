cf6e2032edb1a3282c3f82e6ab698445fca0b51b
fravaccaro <fravaccaro90@gmail.com>
2015-12-08 12:40:48 +0100

    Extensions support added

---
+++ usr/share/harbour-dyncal/harbour-dyncal-uninstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#This script is written by fravaccaro fravaccaro@jollacommunity.it
+#If you want to reuse it, please don't remove this notice
+
+rm -rf /usr/share/themes/jolla-ambient/meegotouch/z1.0/icons/icon-launcher-calendar.png
+cp /usr/share/harbour-dyncal/icon-launcher-calendar.png /usr/share/themes/jolla-ambient/meegotouch/z1.0/icons/icon-launcher-calendar.png
+rm -rf /usr/share/applications/jolla-calendar.desktop
+/usr/bin/desktop-file-install /usr/share/applications/jolla-calendar.desktop
+echo '[Desktop Entry]
+Type=Application
+Name=Calendar
+X-MeeGo-Logical-Id=calendar-ap-name
+X-MeeGo-Translation-Catalog=calendar
+Exec=invoker --type=silica-qt5 -n -d 5 -s /usr/bin/jolla-calendar
+Comment=Jolla calendar
+X-Maemo-Service=com.jolla.calendar.ui
+X-Maemo-Object-Path=/com/jolla/calendar/ui
+X-Maemo-Method=com.jolla.calendar.ui.activateWindow
+X-Desktop-File-Install-Version=0.20' > /usr/share/applications/jolla-calendar.desktop
+/usr/bin/desktop-file-install /usr/share/applications/jolla-calendar.desktop --set-icon=icon-launcher-calendar
+exit 0
